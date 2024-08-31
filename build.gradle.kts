8e9e3d0edd3b7c39fa824df9f44c985fd8ffc138
Ryotaro Sakai <sakai@cloudbuilders.jp>
2024-08-31 17:13:28 +0900

    依存関係追加

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,21 +1,39 @@
 plugins {
     kotlin("jvm") version "2.0.10"
+    id("org.springframework.boot") version "3.3.3"
+    id("io.spring.dependency-management") version "1.1.6"
+    id("com.gradleup.shadow") version "8.3.0"
+    id("org.barfuin.gradle.taskinfo") version "2.2.0"
 }
 
 group = "org.example"
-version = "1.0-SNAPSHOT"
+version = "0.1"
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+        vendor.set(JvmVendorSpec.AMAZON)
+    }
+}
 
 repositories {
     mavenCentral()
 }
 
+val kotlinxSerializationVersion = "1.3.2"
+val springCloudFunctionVersion = "4.1.3"
+
 dependencies {
-    testImplementation(kotlin("test"))
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.cloud:spring-cloud-function-kotlin:$springCloudFunctionVersion")
+    implementation("org.springframework.cloud:spring-cloud-function-adapter-aws:$springCloudFunctionVersion")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$kotlinxSerializationVersion")
 }
 
-tasks.test {
-    useJUnitPlatform()
+tasks.shadowJar {
+    archiveClassifier.set("aws")
 }
-kotlin {
-    jvmToolchain(17)
+
+tasks.assemble {
+    dependsOn(tasks.shadowJar)
 }
